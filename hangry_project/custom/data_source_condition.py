92dce88da4b19e0a136ae3e04a49c4371c0414e7
Jauhar-Hakim <root@3fc52a6ae64b>
2024-09-01 11:00:07 +0000

    add conditional block

---
+++ hangry_project/custom/data_source_condition.py
@@ -0,0 +1,9 @@
+if 'custom' not in globals():
+    from mage_ai.data_preparation.decorators import custom
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@custom
+def transform_custom(*args, **kwargs):
+    return kwargs['source']
