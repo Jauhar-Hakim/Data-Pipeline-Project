d02ba8c07111395c2f9b2931fd565a5821cdcc80
Jauhar-Hakim <root@3fc52a6ae64b>
2024-09-01 10:12:59 +0000

    update data local load

--- hangry_project/data_loaders/data_load_local.py
+++ hangry_project/data_loaders/data_load_local.py
@@ -1,4 +1,7 @@
-from mage_ai.io.file import FileIO
+import io
+import pandas as pd
+import json
+
 if 'data_loader' not in globals():
     from mage_ai.data_preparation.decorators import data_loader
 if 'test' not in globals():
@@ -16,9 +19,12 @@ def load_data_from_file(*args, **kwargs):
 
     Docs: https://docs.mage.ai/design/data-loading#fileio
     """
-    filepath = 'path/to/your/file.csv'
+    file_directories = 'newdata'
+    df_menu = pd.read_csv(file_directories+'/Menu.csv')
+    df_order = pd.read_csv(file_directories+'/Order.csv')
+    df_promotion = pd.read_csv(file_directories+'/Promotion.csv')
 
-    return FileIO().load(filepath)
+    return {'df_menu':df_menu.to_json(orient='records'),'df_order':df_order.to_json(orient='records'),'df_promotion':df_promotion.to_json(orient='records')}
 
 
 @test
